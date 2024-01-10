0bfc8f52a364c58da6627be93a5e988d28397b39
ryandord <84190123+ryandord@users.noreply.github.com>
2024-01-10 13:05:32 +0100

    déplacement + refacto

---
+++ FrontEnd/Quasar/src/pages/LoginPage.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+let userName = ref();
+let password = ref();
+
+</script>
+
+<template>
+  <div class="q-pa-md form">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input
+        outlined
+        v-model="userName"
+        label="Nom d'utilisateur"
+      />
+      <q-input
+        outlined
+        v-model="password"
+        label="Mot de passe"
+        type="password"
+      />
+      <q-btn
+        text-color="primary"
+        unelevated
+        outline
+        label="Connexion avec Google"
+      />
+      <q-btn
+        color="primary"
+        text-color="white"
+        unelevated
+        label="Se connecter"
+      />
+      <br>
+      <a href="/register">Je suis nouveau</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ FrontEnd/Quasar/src/pages/RegisterPage.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import {DefaultUser} from "src/interfaces/user.interface";
+
+let newUser = ref(DefaultUser());
+let pass = ref();
+let passConfirmation = ref()
+
+</script>
+
+<template>
+  <div class="q-pa-md form">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-form>
+        <q-input
+          outlined
+          v-model="newUser.firstName"
+          label="Nom"
+        />
+        <q-input
+          outlined
+          v-model="newUser.lastName"
+          label="Prénom"
+        />
+        <q-input
+          outlined
+          v-model="newUser.email"
+          label="Adresse email"
+          type="email"
+        />
+        <q-input
+          outlined
+          v-model="newUser.username"
+          label="Nom d'utilisateur"
+        />
+        <q-input
+          outlined
+          v-model="pass"
+          label="Mot de passe"
+          type="password"
+        />
+        <q-input
+          outlined
+          v-model="passConfirmation"
+          label="Confirmer"
+          type="password"
+        />
+        <q-btn
+          text-color="primary"
+          unelevated
+          outline
+          label="Connexion avec Google"
+        />
+      </q-form>
+      <q-btn
+        color="primary"
+        text-color="white"
+        unelevated
+        label="Je créé mon compte"
+      />
+      <br>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
