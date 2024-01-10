c0ac97fc378b056a68e0988b0ba7b34997e4aae8
Tristan Tourbier <tristan.tourbier@outlook.fr>
2024-01-10 15:15:51 +0100

    1ere rout e fait

---
+++ BackEnd/Express/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+const Joi = require('joi');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+async function CreateUser(req, res){
+
+    try {
+
+
+        const testRyan = {
+            "l1":"on est la",
+            "l2":"on est la",
+            "l3":"on est la",
+        }
+
+        return res.status(418).json({ json: testRyan });
+
+    } catch (error) {
+
+        return res.status(500).json({ error: "error." });
+
+    }
+
+}
+
+async function LoginUser(req,res){
+
+    try {
+
+        return res.status(418).json({ error: "on est la" });
+
+    }catch (error){
+
+    }
+
+}
+
+
+module.exports = {
+    CreateUser,
+    LoginUser
+};
