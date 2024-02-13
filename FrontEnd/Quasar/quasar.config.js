ac3ecffe1675836b3c7c00003b61243ba92fc1d7
Tristan Tourbier <tristan.tourbier@outlook.fr>
2024-02-13 13:21:58 +0100

    save

--- FrontEnd/Quasar/quasar.config.js
+++ FrontEnd/Quasar/quasar.config.js
@@ -163,6 +163,8 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-cordova-apps/configuring-cordova
     cordova: {
+
+        appId: 'org.cordova.quasar.app',
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
