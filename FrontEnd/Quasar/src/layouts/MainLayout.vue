1a0e67c0fffd6409514ee453227fb3c29cd38404
ryandord <84190123+ryandord@users.noreply.github.com>
2024-02-14 14:01:48 +0100

    Base Pannel

--- FrontEnd/Quasar/src/layouts/MainLayout.vue
+++ FrontEnd/Quasar/src/layouts/MainLayout.vue
@@ -1,14 +1,9 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+  <q-layout view="lHh LpR lFf">
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+    <left-pannel>
+
+    </left-pannel>
 
     <q-page-container>
       <router-view />
@@ -20,6 +15,7 @@
 
 import {onMounted, ref} from "vue";
 import {useRouter} from "vue-router";
+import LeftPannel from "components/Common/LeftPannel.vue";
 const router = useRouter();
 
 let authToken = ref(localStorage.getItem('authToken'));
