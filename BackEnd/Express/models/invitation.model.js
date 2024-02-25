3bb4cf39dfe21efa13c66cbda109330c71c5677f
Seraphinsupinfo <93323611+Seraphinsupinfo@users.noreply.github.com>
2024-02-25 13:11:52 +0100

    Modèle

---
+++ BackEnd/Express/models/invitation.model.js
@@ -0,0 +1,39 @@
+const { DataTypes, Transaction} = require('sequelize');
+const sequelize = require('../configurations/db.config');
+const GroupModel = require('./group.model');
+
+const InvitationModel = sequelize.define("Invitation", {
+    id : {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    group_id: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    remaining_uses: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    expiration_date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    token: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+});
+
+// Relation avec les groupes
+InvitationModel.belongsTo(GroupModel, { foreignKey: 'group_id' });
+
+sequelize.sync().then(() => {
+    console.log('InvitationModel table created successfully!');
+}).catch((error) => {
+    console.error('Unable to create table UserModel : InvitationModel', error);
+});
+
+module.exports = InvitationModel;
