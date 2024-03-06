9c436b205476cc98797d34b399b1df0db7f354f2
ryandord <84190123+ryandord@users.noreply.github.com>
2024-03-06 13:05:03 +0100

    homepage v2

---
+++ FrontEnd/Quasar/src/components/Index/TotalPaid.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <q-card class="bloc-paye bg-primary">
+    <q-card-section >
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar rounded color="secondary" text-color="white" icon="south_west" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-grey-5">Total payé</q-item-label>
+          <q-item-label class="text-white" caption lines="2">4567€</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-card-section>
+  </q-card>
+</template>
+
+<style scoped>
+
+.bloc-paye{
+  width: 40%;
+  height: 120px;
+  border-radius: 15px;
+}
+
+</style>
