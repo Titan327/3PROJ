cef4864b4af3d579d1890c14e431c3a0df3cdc6e
Tristan Tourbier <tristan.tourbier@outlook.fr>
2024-03-13 15:18:38 +0100

    s

--- FrontEnd/Quasar/quasar.config.js
+++ FrontEnd/Quasar/quasar.config.js
@@ -89,7 +89,7 @@ module.exports = configure(function (/* ctx */) {
     devServer: {
       // https: true
       open: false, // opens browser window automatically
-      port:9000,
+      port:443,
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
