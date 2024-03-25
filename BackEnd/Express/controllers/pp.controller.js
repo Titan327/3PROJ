bd9154713d640acfbaaa552a544df8b48a8d6968
Tristan Tourbier <tristan.tourbier@outlook.fr>
2024-03-25 13:32:19 +0100

    save

--- BackEnd/Express/controllers/pp.controller.js
+++ BackEnd/Express/controllers/pp.controller.js
@@ -1,18 +1,15 @@
 const { minioClient } = require('../configurations/minio.config');
 const User = require("../models/user.model");
 
-const uploadPic = async (req, res) => {
+const uploadUserPic = async (req, res) => {
 
     try {
-
         const tokenValue = req.authorization
-
 
         if (!req.resizedImages) {
             return res.status(400).json({ error: 'Aucune image redimensionnée trouvée' });
         }
 
-        let i = 1;
         req.resizedImages.forEach(image => {
 
             /*
@@ -30,7 +27,7 @@ const uploadPic = async (req, res) => {
 
 }
 
-const GetPic = async (req, res) => {
+const GetUserPic = async (req, res) => {
     const bucketName = 'pp-user';
     const userId = req.params.userId;
     const size = req.params.size;
@@ -52,7 +49,56 @@ const GetPic = async (req, res) => {
     }
 }
 
+
+const uploadGroupPic = async (req, res) => {
+
+    try {
+        const tokenValue = req.authorization
+
+        if (!req.resizedImages) {
+            return res.status(400).json({ error: 'Aucune image redimensionnée trouvée' });
+        }
+
+        req.resizedImages.forEach(image => {
+
+            /*
+            const metaData = {
+                'Content-Type': 'jpg',
+            };
+            */
+            minioClient.putObject("pp-group", tokenValue.user_id+'/'+image.name, image.buffer);
+        });
+
+        res.status(200).json({ message: 'photo de profile upload' });
+    }catch (e){
+        res.status(500).json({ error: 'Erreur lors du redimensionnement des images' });
+    }
+
+}
+
+const GetGroupPic = async (req, res) => {
+    const bucketName = 'pp-group';
+    const userId = req.params.userId;
+    const size = req.params.size;
+    const existing = await User.findOne({
+        where: {
+            id: userId,
+        },
+    });
+
+    if (existing){
+        await minioClient.getObject(bucketName, userId+'/'+size, (err, dataStream) => {
+            if (err) {
+                return res.status(500).json({ error: "Une erreur interne au serveur est surenue, veuiller contacter les administrateurs" });
+            }
+            dataStream.pipe(res);
+        });
+    }else {
+        return res.status(449).json({ error: "Gare innexistante" });
+    }
+}
+
 module.exports={
-    uploadPic,
-    GetPic
+    uploadUserPic,
+    GetUserPic
 }
--- BackEnd/Express/routes/image.route.js
+++ BackEnd/Express/routes/image.route.js
@@ -6,7 +6,10 @@ const securityMiddleware = require("../security/middleware.security");
 const upload = multer();
 
 //POST http://localhost:9002/api/img/test
-router.post("/upload/profile-picture", securityMiddleware.verifyIsAuth, upload.single('image'),resize,pp.uploadPic);
-router.get("/profile-picture/:userId/:size",pp.GetPic);
+router.post("/upload/profile-picture", securityMiddleware.verifyIsAuth, upload.single('image'),resize,pp.uploadUserPic);
+router.get("/profile-picture/:userId/:size",pp.GetUserPic);
+
+router.post("/upload/group-picture", securityMiddleware.verifyIsAuth, upload.single('image'),resize,pp.uploadUserPic);
+router.get("/group-picture/:userId/:size",pp.GetUserPic);
 
 module.exports = router;
