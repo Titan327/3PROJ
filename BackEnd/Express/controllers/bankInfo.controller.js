cb867a0484ee797dde8e9b6f0bb82508109b01cb
Tristan Tourbier <tristan.tourbier@outlook.fr>
2024-04-22 18:55:22 +0200

    get all ou get by id

--- BackEnd/Express/controllers/bankInfo.controller.js
+++ BackEnd/Express/controllers/bankInfo.controller.js
@@ -1,7 +1,6 @@
 const BankInfo = require("../models/bankInfo.model")
 
 const Get = async (req,res) => {
-
     try {
         const bankInfo = await BankInfo.find();
         return res.status(200).send(bankInfo);
@@ -9,9 +8,20 @@ const Get = async (req,res) => {
         console.error(e);
         return e;
     }
+}
 
+const GetByCodeBanque = async (req,res) => {
+    try {
+        const {code_banque} = req.params
+        const bankInfo = await BankInfo.find({code_banque:code_banque});
+        return res.status(200).send(bankInfo);
+    } catch (e) {
+        console.error(e);
+        return e;
+    }
 }
 
 module.exports={
-    Get
+    Get,
+    GetByCodeBanque
 }
--- BackEnd/Express/routes/bankInfo.route.js
+++ BackEnd/Express/routes/bankInfo.route.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const securityMiddleware = require('../security/middleware.security')
 const bankInfo = require('../controllers/bankInfo.controller')
 
+//GET http://localhost:9000/api/bankInfo/all
+router.get("/all",securityMiddleware.verifyIsAuth,bankInfo.Get);
 //GET http://localhost:9000/api/bankInfo/
-router.get("/",securityMiddleware.verifyIsAuth,bankInfo.Get);
+router.get("/:code_banque",securityMiddleware.verifyIsAuth,bankInfo.GetByCodeBanque);
 
 
 module.exports = router;
