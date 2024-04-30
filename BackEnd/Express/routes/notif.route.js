dba9849452dad54ac97ff80abb7ef551a8beeeed
Tristan Tourbier <tristan.tourbier@outlook.fr>
2024-04-30 11:02:50 +0200

    done

--- BackEnd/Express/routes/notif.route.js
+++ BackEnd/Express/routes/notif.route.js
@@ -3,6 +3,7 @@ const securityMiddleware = require('../security/middleware.security');
 const notif = require('../controllers/notif.controller');
 
 router.get("/", securityMiddleware.verifyIsAuth, notif.GetAllNotifByUser);
+router.delete("/", securityMiddleware.verifyIsAuth, notif.DeleteNotifById);
 router.get("/count", securityMiddleware.verifyIsAuth, notif.GetNumNotifByUser);
 
 module.exports = router;
