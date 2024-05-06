70a30cf19c46aafe348bd061f2ef90bda1ff4544
ryandord <84190123+ryandord@users.noreply.github.com>
2024-05-06 14:56:37 +0200

    Statistiques

--- FrontEnd/Quasar/src/interfaces/userStatistics.interface.ts
+++ FrontEnd/Quasar/src/interfaces/userStatistics.interface.ts
@@ -1,10 +1,10 @@
-export interface UserStatistics {
+export interface UserTransactionsStatistics {
   statistic: string;
-  average?: string;
-  transactions?: number;
-  amountByCategories?: Record<string, number>;
-  numberByCategories?: Record<string, number>;
-  averageByCategories?: Record<string, string>;
-  amountByMonth?: Record<string, number>;
-  numberByMonth?: Record<string, number>;
+  average: string;
+  transactions: number;
+  amountByCategories: Record<string, number>;
+  numberByCategories: Record<string, number>;
+  averageByCategories: Record<string, string>;
+  amountByMonth: Record<string, number>;
+  numberByMonth: Record<string, number>;
 }
