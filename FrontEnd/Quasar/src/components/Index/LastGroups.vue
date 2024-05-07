79e99437ece7992e162d7b8daf0259dffdf05702
ryandord <84190123+ryandord@users.noreply.github.com>
2024-05-07 09:59:32 +0200

    Filtre lastGroups

--- FrontEnd/Quasar/src/components/Index/LastGroups.vue
+++ FrontEnd/Quasar/src/components/Index/LastGroups.vue
@@ -1,55 +1,105 @@
 <script setup lang="ts">
-
-import {onMounted, ref } from "vue";
-import {Group} from "src/interfaces/group.interface";
-import {api} from "boot/axios";
-import {getUser} from "stores/userStore";
-import {useRouter} from "vue-router";
+import { onMounted, ref } from "vue";
+import { Group } from "src/interfaces/group.interface";
+import { api } from "boot/axios";
+import { getUser } from "stores/userStore";
+import { useRouter } from "vue-router";
+import DialogCreateGroup from "components/Groups/DialogCreateGroup.vue";
+import { useQuasar } from "quasar";
 
 let groupList = ref<Group[]>([]);
 const router = useRouter();
+let dialogCreate = ref(false);
+const $q = useQuasar();
+let favEnabled = ref(false);
 
 onMounted(async () => {
-
-  const userData = await getUser();
-
-  try {
-    const response = await api.get(`/users/${userData.id}/groups?limit=5`);
-    groupList.value = response.data;
-  }
-  catch (e) {
-    console.error(e);
-  }
-
+  await getGroups();
 });
 
-</script>
+async function getGroups(fav = false, set = false) {
+  if (!set) {
+    fav = localStorage.getItem("fav-homepage") === "true";
+  }
+  if (set) {
+    localStorage.setItem("fav-homepage", fav.toString());
+  }
+  favEnabled.value = fav;
+  const userData = await getUser();
+  try {
+    const response = await api.get(`/users/${userData.id}/groups`, {
+      params: {
+        favorite: fav,
+        limit: 10,
+      },
+    });
+    groupList.value = response.data;
+  } catch (e) {
+    console.error(e);
+  }
+}
 
+function openDialogCreate() {
+  dialogCreate.value = true;
+  $q.dialog({
+    component: DialogCreateGroup,
+    componentProps: {
+      isOpen: dialogCreate,
+    },
+  }).onDismiss(() => {
+    getGroups();
+  });
+}
+</script>
 
 <template>
   <q-card class="bloc-group bg-accent">
     <q-card-section>
-      <h3 class="text-h6">Mes Groupes</h3>
+      <div class="row">
+        <h3 class="text-h6">Mes Groupes
+          <q-badge color="secondary" floating>
+            <q-icon v-if="favEnabled" name="star" color="white" />
+            <q-icon v-else name="history" color="white" />
+          </q-badge>
+        </h3>
+        <q-space />
+        <q-icon name="more_horiz" size="md" color="secondary" class="q-pa-md">
+          <q-menu auto-close dark>
+            <q-list style="min-width: 50px">
+              <q-item clickable @click="getGroups(true, true)">
+                <q-item-section>Afficher uniquement les favoris</q-item-section>
+              </q-item>
+              <q-item clickable @click="getGroups(false, true)">
+                <q-item-section>Afficher les derniers groupes</q-item-section>
+              </q-item>
+              <q-separator color="accent" />
+              <q-item clickable @click="openDialogCreate()">
+                <q-item-section>Créer un groupe</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-icon>
+      </div>
     </q-card-section>
     <q-card-section>
       <div class="q-gutter-md q-ml-none">
         <q-avatar
-            v-for="group in groupList" :key="group.id"
-            size="100px"
-            @click="router.push(`/groups/${group.id}`)"
-          >
-            <img :src="group.picture ? group.picture+'/200' : 'assets/defaults/group-default.webp'">
-            <q-badge color="red" rounded floating >{{group.id}}</q-badge>
-          </q-avatar>
+          v-for="group in groupList"
+          :key="group.id"
+          size="100px"
+          @click="router.push(`/groups/${group.id}`)"
+        >
+          <img :src="group.picture ? group.picture[1] : 'assets/defaults/group-default.webp'" />
+          <q-badge color="red" floating>{{ group.id }}</q-badge>
+        </q-avatar>
       </div>
     </q-card-section>
-    <q-separator/>
+    <q-separator />
   </q-card>
 </template>
 
 <style scoped>
-
-.bloc-group{
+.bloc-group {
   width: 100%;
   border-radius: 15px;
   margin-top: 50px;
