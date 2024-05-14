5d9599f9a27e9b5137aa7a032232d3afbb9fd17a
Seraphinsupinfo <93323611+Seraphinsupinfo@users.noreply.github.com>
2024-05-14 21:18:26 +0200

    On peut récupérer tous les groupes dans la liste

--- AndroidApp/app/src/main/java/sample_test_app/com/ui/screens/GroupListScreen.kt
+++ AndroidApp/app/src/main/java/sample_test_app/com/ui/screens/GroupListScreen.kt
@@ -1,46 +1,36 @@
 package sample_test_app.com.ui.screens
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import io.ktor.client.*
-import io.ktor.util.InternalAPI
-import sample_test_app.com.http.Repository.GroupRepository
-import sample_test_app.com.models.Group
-import kotlinx.serialization.Serializable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.IconButton
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -52,11 +42,8 @@ import kotlinx.coroutines.launch
 import sample_test_app.com.LocalJwtToken
 import sample_test_app.com.LocalUser
 import sample_test_app.com.R
-import sample_test_app.com.models.TransactionUser
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.unit.sp
+import sample_test_app.com.http.Repository.GroupRepository
+import sample_test_app.com.models.Group
 
 @Composable
 fun GroupListScreen(httpClient: HttpClient, navController: NavController) {
@@ -71,8 +58,7 @@ fun GroupListScreen(httpClient: HttpClient, navController: NavController) {
 
     LaunchedEffect(key1 = userId) {
         CoroutineScope(Dispatchers.Main).launch {
-            groups.value = groupRepository.getUserGroups(userId, jwtToken, true)
-            println("Received token: ${jwtToken}")
+            groups.value = groupRepository.getUserGroups(userId, jwtToken, false)
         }
     }
 
@@ -137,7 +123,7 @@ fun GroupListScreen(httpClient: HttpClient, navController: NavController) {
                                 CoroutineScope(Dispatchers.Main).launch {
                                     groupRepository.createGroup(jwtToken, groupName.value, groupDescription.value)
                                     showDialog.value = false
-                                    groups.value = groupRepository.getUserGroups(userId, jwtToken, true)
+                                    groups.value = groupRepository.getUserGroups(userId, jwtToken, false)
                                 }
                             }) {
                                 Text("Create Group")
