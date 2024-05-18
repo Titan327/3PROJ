20b07f7bf4a2227804077ff5abe5de605538fc97
Thomas <thomas.lapere@supinfo.com>
2024-05-19 01:37:47 +0200

    modif style

--- AndroidApp/app/src/main/java/sample_test_app/com/ui/screens/LoginScreen.kt
+++ AndroidApp/app/src/main/java/sample_test_app/com/ui/screens/LoginScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -45,6 +46,7 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import sample_test_app.com.R
 import sample_test_app.com.http.Security.JwtUtils.JwtUtils
 import sample_test_app.com.models.User
 
@@ -77,12 +79,16 @@ fun LoginScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.logobig),
+            contentDescription = "Logo"
+        )
         TextField(
             value = usernameState.value,
             onValueChange = { newValue ->
                 usernameState.value = newValue
             },
-            placeholder = { Text("Enter your username") },
+            placeholder = { Text("Entrer votre pseudo") },
             colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White),
             modifier = Modifier.padding(vertical = 8.dp)
         )
@@ -92,7 +98,7 @@ fun LoginScreen(
             onValueChange = { newValue ->
                 passwordState.value = newValue
             },
-            placeholder = { Text("Enter your password") },
+            placeholder = { Text("Entrer votre mot de passe") },
             visualTransformation = PasswordVisualTransformation(),
             colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White),
             modifier = Modifier.padding(vertical = 8.dp)
@@ -165,7 +171,7 @@ fun LoginScreen(
 
 
         ) {
-            Text("Login", color = Color.White)
+            Text("Connexion", color = Color.White)
 
 
         }
